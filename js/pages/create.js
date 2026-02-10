5a35a621c668d620625a61ca883ff2016a6850d4
8headswillroll8 <lejohansen91@gmail.com>
2026-02-10 22:06:07 +0100

    apply base CSS across all pages

--- js/pages/create.js
+++ js/pages/create.js
@@ -1,1 +1,7 @@
 import "../ui/header.js";
+
+const title = document.getElementById("title");
+const tag = document.getElementById("tag");
+const image = document.getElementById("image");
+const body = document.getElementById("body");
+const publishBtn = document.getElementById("publish-btn");
